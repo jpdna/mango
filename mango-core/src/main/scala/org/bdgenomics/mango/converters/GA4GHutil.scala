9c1a3e3fd3726998a9d89a71b1112cfdea543c21
Paschall <paschalj@HG-01931188-LM1.local>
2017-09-11 15:02:43 -0400

    removed old unneeded converter code

--- mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
@@ -11,7 +11,6 @@ import org.bdgenomics.convert.ga4gh.Ga4ghModule
 import org.bdgenomics.convert.{ ConversionStringency, Converter }
 import org.bdgenomics.formats.avro.AlignmentRecord
 import org.bdgenomics.mango.converters
-import org.bdgenomics.mango.converters.AlignmentRecordConverterGA4GH.bdgToGA4GH
 
 import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
@@ -27,11 +26,8 @@ object GA4GHutil {
   val converter: Converter[AlignmentRecord, ReadAlignment] = injector.getInstance(Key.get(new TypeLiteral[Converter[AlignmentRecord, ReadAlignment]]() {}))
   def alignmentRecordRDDtoJSON(alignmentRecordRDD: AlignmentRecordRDD): String = {
     val x: Array[AlignmentRecord] = alignmentRecordRDD.rdd.collect()
-
     val logger = LoggerFactory.getLogger("GA4GHutil")
-
     val gaReads = x.map(a => converter.convert(a, ConversionStringency.LENIENT, logger))
-
     val result: ga4gh.ReadServiceOuterClass.SearchReadsResponse = ga4gh.ReadServiceOuterClass.SearchReadsResponse.newBuilder().addAllAlignments(gaReads.toList.asJava).build()
     com.google.protobuf.util.JsonFormat.printer().includingDefaultValueFields().print(result)
   }
