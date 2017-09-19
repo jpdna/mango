e2558acc9a493c9fa5fd4a5d192d60e651df03f9
Paschall <paschalj@od01926020.cit.nih.gov>
2017-09-19 11:00:56 -0400

    moved ga4gh converter map before collect

--- mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
+++ mango-core/src/main/scala/org/bdgenomics/mango/converters/GA4GHutil.scala
@@ -4,12 +4,17 @@ import javax.inject.Inject
 
 import com.google.inject._
 import ga4gh.Reads.ReadAlignment
+import ga4gh.Variants.Variant
 import net.codingwell.scalaguice.ScalaModule
+import org.apache.spark.rdd.RDD
 import org.bdgenomics.adam.models.ReferenceRegion
 import org.bdgenomics.adam.rdd.read.AlignmentRecordRDD
+import org.bdgenomics.adam.rdd.variant.{ GenotypeRDD, VariantContextRDD }
+import org.bdgenomics.adam.models.VariantContext
 import org.bdgenomics.convert.ga4gh.Ga4ghModule
 import org.bdgenomics.convert.{ ConversionStringency, Converter }
 import org.bdgenomics.formats.avro.AlignmentRecord
+import org.bdgenomics.formats.avro.Variant
 import org.bdgenomics.mango.converters
 
 import scala.collection.JavaConverters._
@@ -23,12 +28,44 @@ import org.slf4j.LoggerFactory
 
 object GA4GHutil {
   val injector: Injector = Guice.createInjector(new Ga4ghModule())
-  val converter: Converter[AlignmentRecord, ReadAlignment] = injector.getInstance(Key.get(new TypeLiteral[Converter[AlignmentRecord, ReadAlignment]]() {}))
+
+  val alignmentConverter: Converter[AlignmentRecord, ReadAlignment] = injector
+    .getInstance(Key.get(new TypeLiteral[Converter[AlignmentRecord, ReadAlignment]]() {}))
+
+  val variantConverter: Converter[org.bdgenomics.formats.avro.Variant, ga4gh.Variants.Variant] = injector
+    .getInstance(Key.get(new TypeLiteral[Converter[org.bdgenomics.formats.avro.Variant, ga4gh.Variants.Variant]]() {}))
+
+  val genotypeConverter: Converter[org.bdgenomics.formats.avro.Genotype, ga4gh.Variants.Call] = injector
+    .getInstance(Key.get(new TypeLiteral[Converter[org.bdgenomics.formats.avro.Genotype, ga4gh.Variants.Call]]() {}))
+
   def alignmentRecordRDDtoJSON(alignmentRecordRDD: AlignmentRecordRDD): String = {
-    val x: Array[AlignmentRecord] = alignmentRecordRDD.rdd.collect()
     val logger = LoggerFactory.getLogger("GA4GHutil")
-    val gaReads = x.map(a => converter.convert(a, ConversionStringency.LENIENT, logger))
-    val result: ga4gh.ReadServiceOuterClass.SearchReadsResponse = ga4gh.ReadServiceOuterClass.SearchReadsResponse.newBuilder().addAllAlignments(gaReads.toList.asJava).build()
+    val gaReads: Array[ReadAlignment] = alignmentRecordRDD.rdd.map(a => alignmentConverter
+      .convert(a, ConversionStringency.LENIENT, logger))
+      .collect()
+
+    val result: ga4gh.ReadServiceOuterClass.SearchReadsResponse = ga4gh.ReadServiceOuterClass.SearchReadsResponse.newBuilder()
+      .addAllAlignments(gaReads.toList.asJava).build()
+
     com.google.protobuf.util.JsonFormat.printer().includingDefaultValueFields().print(result)
+  }
+
+  def variantContextRDDtoJSON(variantContextRDD: VariantContextRDD): String = {
+    val logger = LoggerFactory.getLogger("GA4GHutil")
+
+    val gaVariants: Array[ga4gh.Variants.Variant] = variantContextRDD.rdd.map(a => {
+      ga4gh.Variants.Variant.newBuilder(variantConverter.convert(a.variant.variant, ConversionStringency.LENIENT, logger))
+        .addAllCalls(a.genotypes.map((g) => genotypeConverter.convert(g, ConversionStringency.LENIENT, logger)).asJava)
+        .build()
+    }).collect()
+
+    val result: ga4gh.VariantServiceOuterClass.SearchVariantsResponse = ga4gh.VariantServiceOuterClass
+      .SearchVariantsResponse.newBuilder().addAllVariants(gaVariants.toList.asJava).build()
+
+    com.google.protobuf.util.JsonFormat.printer().includingDefaultValueFields().print(result)
+  }
+
+  def genotypeRDDtoJSON(genotypeRDD: GenotypeRDD): String = {
+    variantContextRDDtoJSON(genotypeRDD.toVariantContextRDD())
   }
 }
